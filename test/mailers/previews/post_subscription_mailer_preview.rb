c4c95b698fec3f8bb80e3321938c4e7fe9cd8887
Luke Gruber <luke.gru@gmail.com>
2024-10-23 17:39:53 -0400

    add PostSubscriptionMailerPreview

--- test/mailers/previews/post_subscription_mailer_preview.rb
+++ test/mailers/previews/post_subscription_mailer_preview.rb
@@ -1,3 +1,14 @@
 # Preview all emails at http://localhost:3000/rails/mailers/post_subscription_mailer
 class PostSubscriptionMailerPreview < ActionMailer::Preview
+  def email_subscriber
+    post = Post.find_by_id(3)
+    post = Post.last unless post
+    content = post.erb_content(content: post.content_with_wrapper)
+    PostSubscriptionMailer.with(
+      post_id: post.id,
+      post_title: post.title,
+      content: content,
+      email: "i@subscribed.com",
+    ).email_subscriber
+  end
 end
