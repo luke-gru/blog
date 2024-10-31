9a6128a2c3a1f0ed8386d199de342352a431e62a
Luke Gruber <luke.gru@gmail.com>
2024-10-31 15:02:17 -0400

    get sidekiq and redis configurations working

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,27 @@
 if Rails.env.development?
   Sidekiq.configure_server do |config|
     config.redis = {
-      host: ENV['REDIS_HOST'] || 'localhost',
-      port: ENV['REDIS_PORT'] || '6379'
+      url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0",
+    }
+  end
+  Sidekiq.configure_client do |config|
+    config.redis = {
+      url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0",
+    }
+  end
+elsif Rails.env.production?
+  Sidekiq.configure_server do |config|
+    if ENV['REDIS_HOST'].blank?
+      $stderr.puts "no redis host set!"
+      exit 1
+    end
+    config.redis = {
+      url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0",
+    }
+  end
+  Sidekiq.configure_client do |config|
+    config.redis = {
+      url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0",
     }
   end
 end
