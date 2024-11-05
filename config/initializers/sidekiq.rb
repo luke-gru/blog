ef2487ef660916eaa6f0b3e75ac67b03e27ba5c0
Luke Gruber <luke.gru@gmail.com>
2024-11-05 11:35:24 -0500

    add a password for redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -12,16 +12,20 @@ if Rails.env.development?
 elsif Rails.env.production?
   Sidekiq.configure_server do |config|
     if ENV['REDIS_HOST'].blank?
-      $stderr.puts "no redis host set!"
+      $stderr.puts "no redis host set! (REDIS_HOST)"
+      exit 1
+    end
+    if ENV['BLOG_REDIS_PASS'].blank?
+      $stderr.puts "no redis password set! (BLOG_REDIS_PASS)"
       exit 1
     end
     config.redis = {
-      url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0",
+      url: "redis://#{ENV['BLOG_REDIS_PASS']}@#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0",
     }
   end
   Sidekiq.configure_client do |config|
     config.redis = {
-      url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0",
+      url: "redis://#{ENV['BLOG_REDIS_PASS']}@#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0",
     }
   end
 end
