d104c1a6adc113ee83ce24ba1b4706fab1258611
Luke Gruber <luke.gru@gmail.com>
2024-11-06 17:37:05 -0500

    add css_compressor = nil in all environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  config.assets.css_compressor = nil # setting to nil because of tailwindcss-rails bug
 
   # Raises error for missing translations.
   config.i18n.raise_on_missing_translations = true
